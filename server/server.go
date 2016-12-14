2820a661fcfdf1bda7ed0380cf9d824cb72889d1
Tim Schindler <xh3b4sd@gmail.com>
2016-12-14 20:34:02 +0100

    wrapped custom error encoder (#12)

--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -139,7 +141,16 @@ func (s *server) Endpoints() []Endpoint {
 }
 
 func (s *server) ErrorEncoder() kithttp.ErrorEncoder {
-	return s.errorEncoder
+	return func(ctx context.Context, err error, w http.ResponseWriter) {
+		s.errorEncoder(ctx, err, w)
+
+		s.Logger.Log("error", fmt.Sprintf("%#v", err))
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err.Error(),
+		})
+	}
 }
 
 // NewRouter returns a HTTP handler for the server. Here we register all
