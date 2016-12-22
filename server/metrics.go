654b8318a4bc4feb6e4bb8b15eabc97683d3ea5f
Tim Schindler <xh3b4sd@gmail.com>
2016-12-22 03:18:52 +0100

    extended error handling

--- server/metrics.go
+++ server/metrics.go
@@ -10,21 +10,21 @@ var (
 			Name: "endpoint_total",
 			Help: "Number of times we have execute the HTTP handler of an endpoint",
 		},
-		[]string{"method", "endpoint", "code"},
+		[]string{"code", "method", "name"},
 	)
 	endpointTime = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "endpoint_milliseconds",
 			Help: "Time taken to execute the HTTP handler of an endpoint, in milliseconds",
 		},
-		[]string{"method", "endpoint", "code"},
+		[]string{"code", "method", "name"},
 	)
 	errorTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "error_total",
-			Help: "Number of times we have seen a specific error",
+			Help: "Number of times we have seen a specific error within a specific error domain",
 		},
-		[]string{"type"},
+		[]string{"domain"},
 	)
 )
 
