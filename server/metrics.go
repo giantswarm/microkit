b3eaff6254afa76cdc00824f18ef75a9530f37a5
Tim Schindler <xh3b4sd@gmail.com>
2017-01-09 18:30:03 +0100

    period after sentence

--- server/metrics.go
+++ server/metrics.go
@@ -8,21 +8,21 @@ var (
 	endpointTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "endpoint_total",
-			Help: "Number of times we have execute the HTTP handler of an endpoint",
+			Help: "Number of times we have execute the HTTP handler of an endpoint.",
 		},
 		[]string{"code", "method", "name"},
 	)
 	endpointTime = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "endpoint_milliseconds",
-			Help: "Time taken to execute the HTTP handler of an endpoint, in milliseconds",
+			Help: "Time taken to execute the HTTP handler of an endpoint, in milliseconds.",
 		},
 		[]string{"code", "method", "name"},
 	)
 	errorTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "error_total",
-			Help: "Number of times we have seen a specific error within a specific error domain",
+			Help: "Number of times we have seen a specific error within a specific error domain.",
 		},
 		[]string{"domain"},
 	)
