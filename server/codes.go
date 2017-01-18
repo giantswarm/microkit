c118e14777586746c905422b77ff7d0dd61303ed
Tim Schindler <xh3b4sd@gmail.com>
2017-01-18 11:56:21 +0100

    ordered and commented server codes

--- server/codes.go
+++ server/codes.go
@@ -1,11 +1,19 @@
 package server
 
 var (
-	CodeResourceCreated       = "RESOURCE_CREATED"
-	CodeResourceUpdated       = "RESOURCE_UPDATED"
-	CodeResourceDeleted       = "RESOURCE_DELETED"
-	CodeResourceNotFound      = "RESOURCE_NOT_FOUND"
+	// CodeInvalidCredentials indicates the provided credentials are not valid.
+	CodeInvalidCredentials = "INVALID_CREDENTIALS"
+	// CodePermissionDenied indicates the provided credentials are valid, but the
+	// requested resource requires other permissions.
+	CodePermissionDenied = "PERMISSION_DENIED"
+	// CodeResourceAlreadyExists indicates a resource does already exist.
 	CodeResourceAlreadyExists = "RESOURCE_ALREADY_EXISTS"
-	CodeInvalidCredentials    = "INVALID_CREDENTIALS"
-	CodePermissionDenied      = "PERMISSION_DENIED"
+	// CodeResourceCreated indicates a resource has been created.
+	CodeResourceCreated = "RESOURCE_CREATED"
+	// CodeResourceDeleted indicates a resource has been deleted.
+	CodeResourceDeleted = "RESOURCE_DELETED"
+	// CodeResourceNotFound indicates a resource could not be found.
+	CodeResourceNotFound = "RESOURCE_NOT_FOUND"
+	// CodeResourceUpdated indicates a resource has been updated.
+	CodeResourceUpdated = "RESOURCE_UPDATED"
 )
