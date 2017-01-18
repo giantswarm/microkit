51115dc460fa204cd0e5756668e543cc9edf446c
Tim Schindler <xh3b4sd@gmail.com>
2017-01-18 01:33:43 +0100

    added first draft of response codes in server package

---
+++ server/codes.go
@@ -0,0 +1,11 @@
+package server
+
+var (
+	CodeResourceCreated       = "RESOURCE_CREATED"
+	CodeResourceUpdated       = "RESOURCE_UPDATED"
+	CodeResourceDeleted       = "RESOURCE_DELETED"
+	CodeResourceNotFound      = "RESOURCE_NOT_FOUND"
+	CodeResourceAlreadyExists = "RESOURCE_ALREADY_EXISTS"
+	CodeInvalidCredentials    = "INVALID_CREDENTIALS"
+	CodePermissionDenied      = "PERMISSION_DENIED"
+)
